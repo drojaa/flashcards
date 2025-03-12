6dbdd510eab4e7f7aa96c6e972bffb2fe2294ed1
drojaa <droja@bu.edu>
2025-03-12 00:28:55 -0400

    Added Flashcards to Homepage + Mock Q/A's

--- src/CreateFlashcard.tsx
+++ src/CreateFlashcard.tsx
@@ -1,27 +1,64 @@
 // Create a Flashcard Component
 
 import "./CreateFlashcard.css";
+import { useState } from "react";
+
+
+
 
 export default function CreateFlashcard() {
+
+    const [question, setQuestion] = useState('');
+    const [response, setResponse] = useState('');
+
+    // Creates a New Flashcard Object 
+    const newFlashcard = {
+        question: question,
+        response: response,
+    }
+
+
+    function handleSubmit(e) {
+        e.preventDefault(); // Prevent browser from reloading
+    
+        const form = e.target;
+        const formData = new FormData(form);
+    
+        const formJson = Object.fromEntries(formData.entries());
+
+
+        setQuestion(formJson.question.toString());
+        setResponse(formJson.response.toString());   
+    }
+
   return (
     <>
      <div id="main-contain">
-      <h1>Create Flashcard</h1>
+     <a id="close" href="/">X</a>
+     <h1>Create Flashcard</h1>
+    
+      <form method="post" onSubmit={handleSubmit}> 
       <div id="add-contain">
+
       <div id="question-contain">
       <label id="create-flash">Question:</label>
-      <textarea id="textarea" name="question" rows={4} cols={40} />
+      <textarea id="textarea" name="question" rows={4} cols={40} placeholder="Write on the front of the card here"/>
       </div>
 
-    <div id="response-contain">
-    <label id="create-flash">Response:</label>
-    <textarea id="textarea" name="response" rows={4} cols={40} />
-    </div>
+      <div id="response-contain">
+      <label id="create-flash">Response:</label>
+      <textarea id="textarea" name="response" rows={4} cols={40} placeholder="Write on the back of the card here" />
+      </div>
 
-    <button id="save">save</button>
-    <button id="save">close</button>
-    </div>
-    </div>
+      <button type="submit" id="save">save</button>
+      
+     
+
+      </div>
+
+      </form>
+      </div>
     </>
   );
+  
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,25 @@
 
 // Compoennt for Homepage 
-
+import { useState } from "react";
+import "./Home.css"
 
 export default function Home() {
+    const [flashcards, setFlashcards] = useState([  { question: "What is the capital of France?", answer: "Paris" }, { question: "Who wrote 'To Kill a Mockingbird'?", answer: "Harper Lee" },
+        { question: "What does HTML standfor?", answer: "HyperText Markup Language" },
+    ]);
+
     return (
       <>
-        <h1>Home Page </h1>
+        <h1 id="home-h1">Home Page </h1>
+        <ul id="home-ul">
+            {flashcards.map((flashcard, index) => (
+                <li id="home-li" key={index}>
+                    <p id="home-p">Q: {flashcard.question}</p>
+                </li>
+
+            ))}
+        </ul>
+
       </>
     );
   }
